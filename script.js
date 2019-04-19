1694daf30790086ba65766e10dc4b8b58230df6f
aledic85 <dicuollo85@gmail.com>
2019-04-19 15:12:45 +0200

    add first section

--- script.js
+++ script.js
@@ -5,11 +5,28 @@ function dropdownMenu() {
   me.siblings(".drop-menu").slideToggle();
 }
 
+function slider() {
+
+  var divActive = $(".slider > div.active");
+
+  divActive.removeClass("active");
+
+  var nextDiv = divActive.next("div");
+
+  if (divActive.hasClass("last")) {
+    nextDiv = $("div.first");
+  }
+
+  nextDiv.addClass("active");
+
+}
+
 function init() {
 
   var order = $("#order");
 
   order.click(dropdownMenu);
+  setInterval(slider, 3000);
 }
 
 $(document).ready(init);
